1197b52745cf2066264d871e3c72e81fa1c57ee6
Laurids <hie17492@spengergasse.at>
2021-04-29 11:16:14 +0200

    Cleanup and rebranding

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Frontpage from './Components/Frontpage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Shop from './Components/Shop';
 
 
 function App() {
@@ -14,16 +12,7 @@ function App() {
         </div>
       </header>
       <div className="container">
-      <Router>
-        <Switch>
-          {/* Frontpage wird automatisch hier als Default geladen
-          Mann muss es nicht extra mit 
-          <Frontpage/>
-          einbinden */}
-          <Route exact path="/" component={Frontpage} />
-          <Route path="/shop" component={Shop} />
-        </Switch>
-      </Router>
+        <Frontpage></Frontpage>
       </div>
     </div>
   );
